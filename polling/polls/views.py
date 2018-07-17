1f0360c0bbcb305e895c969a740c336636ba9bb2
JackBid <jackbidders@gmail.com>
2018-07-17 21:26:21 +0100

    Added question to homepage

--- polling/polls/views.py
+++ polling/polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
 def home(request):
-    return render(request, 'polls/home.html')
+    question_list = Question.objects.order_by('-pub_date')
+    return render(request, 'polls/home.html', {'questions': question_list})
