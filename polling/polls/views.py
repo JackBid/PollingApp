04a2ab0f1c58957611805dac6571beb1514d9c9f
JackBid <jackbidders@gmail.com>
2018-07-17 22:51:00 +0100

    Added choices to questions

--- polling/polls/views.py
+++ polling/polls/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from .models import Question
+from .models import Choice
 
 # Create your views here.
 def home(request):
-    question_list = Question.objects.order_by('-pub_date')
-    return render(request, 'polls/home.html', {'questions': question_list})
+    questions = []
+    for question in Question.objects.all():
+        temp_question = {}
+        temp_question['title'] = question.question_text
+        temp_question['pub_date'] = question.pub_date
+        choicelist = list(Choice.objects.filter(question_id=question.id))
+        temp_question['choices'] = choicelist
+        questions.append(temp_question)
+    return render(request, 'polls/home.html', {'questions':questions})
