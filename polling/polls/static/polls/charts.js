501b45e2aa378393b2a6a4abe90cb89002e1ed0b
JackBid <jackbidders@gmail.com>
2018-07-24 12:17:40 +0100

    Added chart for voting in its own file

---
+++ polling/polls/static/polls/charts.js
@@ -0,0 +1,58 @@
+var CHARTS = CHARTS || (function(){
+  var _args = {};
+  return {
+    init: function(Args){
+      _args = Args;
+    },
+    render: function(){
+      for(var i=0; i<_args.length; i++){
+        var question = _args[i];
+        var labels = [];
+        var data = [];
+        for(var j=0; j<question.choices.length; j++){
+          labels.push(question.choices[j].text);
+          data.push(question.choices[j].votes);
+        }
+        var ctx = $("#chart-" + question.id);
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: '# of Votes',
+                    data: data,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+              responsive: false,
+              maintainAspectRatio: false,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+      }
+    }
+  }
+}());
